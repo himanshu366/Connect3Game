3e6e2a51a9f5ef1c4e5096733686d9aea0b51a3f
Himanshu <himanshuguptax1@gmail.com>
2020-04-01 20:44:30 +0530

    Initial

--- app/src/main/java/com/example/connect3game/MainActivity.java
+++ app/src/main/java/com/example/connect3game/MainActivity.java
@@ -2,12 +2,61 @@ package com.example.connect3game;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.security.PublicKey;
 
 public class MainActivity extends AppCompatActivity {
+    // 0: yellow, 1: red, 2: empty
+    int activePlayer = 0;
+    int[] gameState = {2,2,2,2,2,2,2,2,2};
+    int tappedCounter;
+    boolean gameActive = true;
+    int[][] winningPositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+    public void dropIn(View view) {
+
+        ImageView counter = (ImageView) view;
+        if (gameState[tappedCounter] == 2 && gameActive) {
+
+
+            int tappedCounter = Integer.parseInt(counter.getTag().toString());
+            gameState[tappedCounter] = activePlayer;
+            counter.setTranslationY(-1500);
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+            for (int[] winningPosition : winningPositions) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
+                    //for winning
+                    gameActive = false;
+                    String winner = "";
+                    if (activePlayer == 1) {
+                        winner = "yellow";
+                    } else {
+                        winner = "red";
+                    }
+                    Toast.makeText(this, "Someone has won", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }
+
+        }
+
+
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+
 }
